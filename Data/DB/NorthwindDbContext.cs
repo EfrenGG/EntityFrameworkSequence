d4abe9bd9f8c64c9fd65eb6227ecdee3b3c90352
Efren Garcia Guarneros <efren.garcia.guarneros@gmail.com>
2020-07-16 16:08:53 -0500

    Se agrega context de BD Northwind

---
+++ Data/DB/NorthwindDbContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EntityFrameworkSequence.Models.Entities;
+
+namespace EntityFrameworkSequence.Data.DB
+{
+    public partial class NorthwindDbContext : DbContext
+    {
+        public NorthwindDbContext()
+        {
+        }
+
+        public NorthwindDbContext(DbContextOptions<NorthwindDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.Id).HasDefaultValueSql("(NEXT VALUE FOR [Seq_Person])");
+
+                entity.Property(e => e.EmailAddress)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastUpdatedBy)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastUpdatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ManagerId).HasColumnName("ManagerID");
+
+                entity.Property(e => e.MiddleName).HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.PhoneNumber)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.InverseManager)
+                    .HasForeignKey(d => d.ManagerId)
+                    .HasConstraintName("FK_Person_Person");
+            });
+
+            modelBuilder.HasSequence<int>("Seq_Person");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
